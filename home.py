3b9f031eebb1d7204d43ee2098554ceaaaa7e67b
amanda-matthes <amanda.matthes@gmail.com>
2023-11-15 13:15:04 +0000

    added visible timer

--- home.py
+++ home.py
@@ -11,14 +11,15 @@ import log_tools
 # SETTINGS
 st.set_page_config(layout = 'wide')
 
-timer_column, data_column, garden_column = st.columns([1, 2, 1], gap = 'large')
+timer_column, data_column, garden_column = st.columns([1, 1, 1], gap = 'large')
 
 
 # TIMER
 with timer_column:
     st.write('# timer')
-    mode = st.selectbox('mode', ['stopwatch', 'countdown'], )
 
+    # SELECT MODE
+    mode = st.selectbox('mode', ['stopwatch', 'countdown'], )
     if mode == 'countdown':
         st.write('countdown')
 
@@ -26,47 +27,34 @@ with timer_column:
     else:
         st.write('stopwatch')
 
+        # CONTROL BUTTONS
         start_button = st.button('start')
+        end_button  = st.button('end')
         if start_button:
-            # start_time = time.time()
-            start_time = datetime.strftime(datetime.now(), '%Y-%m-%d_%H:%M:%S')
-            session_tools.update_start_time(start_time)
+            start_time = datetime.now()
+            session_tools.update_start_time(datetime.strftime(start_time, '%Y-%m-%d_%H:%M:%S'))
             session_tools.update_end_time('1900-01-01_00:00:00')
 
-        end_button  = st.button('end')
         if end_button:
-            # end_time = time.time()
-            end_time = datetime.strftime(datetime.now(), '%Y-%m-%d_%H:%M:%S')
-            session_tools.update_end_time(end_time)
+            end_time = datetime.now()
+            session_tools.update_end_time(datetime.strftime(end_time, '%Y-%m-%d_%H:%M:%S'))
 
+        # TIMER
+        if start_button:
+            with st.empty():
+                done = False
+                while not done:
+                    current_time = datetime.now()
+                    elapsed_time = (current_time - start_time).total_seconds()
+                    hours, remainder = divmod(int(elapsed_time), 3600)
+                    minutes, seconds = divmod(remainder, 60)
 
-            # st.write('elapsed time: {}'.format(end_time - start_time))
+                    st.write('# {:02}:{:02}:{:02}'.format(hours, minutes, seconds))
 
-            # st.sidebar.write('starting timer')
+                    if end_button:
+                        done == True
 
-            # with st.empty():
-            #     done = False
-            #     while not done:
-            #         current_time = time.time()
-            #         elapsed_time = current_time - start_time
-            #         hours, remainder = divmod(int(elapsed_time), 3600)
-            #         minutes, seconds = divmod(remainder, 60)
-
-            #         st.write('{:02}:{:02}:{:02}'.format(hours, minutes, seconds))
-
-            #         if end_button:
-            #             st.sidebar.write('stopping timer')
-            #             st.write('whooo')
-            #             # st.session_state['timing'] = False
-            #             time.sleep(5)
-            #             done == True
-
-            #         time.sleep(1)
-            #     st.write('blob')
-
-            # st.write(start_time)
-            # st.write('DONE')
-            # st.button('something')
+                    time.sleep(1)
 
 
 # DATA
@@ -99,8 +87,6 @@ with data_column:
 
     # SAVE TO LOG
     current_time = datetime.now()
-
-    st.write(current_time)
 
     save = st.button('save')
     if save:
