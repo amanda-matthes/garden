f96f78e7f8f05d5ad43e0842eb71c84ebd4ecfbc
amanda-matthes <amanda.matthes@gmail.com>
2023-11-21 18:29:07 +0000

    removed not implemented countdown option

--- home.py
+++ home.py
@@ -21,48 +21,27 @@ with timer_column:
     tags            = tag_tools.get_all_tags()
     selected_tag    = st.selectbox('select tag', tags)
 
-    # SELECT MODE
-    mode = st.selectbox('mode', ['stopwatch', 'countdown'], )
-    if mode == 'countdown':
-        st.write('todo')
+    # TIMER STATUS
+    status = st.empty()
+
+    current_start_time = session_tools.get_start_time_string()
+    if current_start_time != '1900-01-01_00:00:00':
+        status.text('timer started at ' + current_start_time)
     else:
-        status = st.empty()
+        status.text('no timer running')
 
-        current_start_time = session_tools.get_start_time_string()
-        if current_start_time != '1900-01-01_00:00:00':
-            status.text('timer started at ' + current_start_time)
-        else:
-            status.text('no timer running')
-
-        # CONTROL BUTTONS
-        start_button    = st.button('start')
-        end_button      = st.button('end')
-        if start_button:
-            start_time = datetime.now()
-            session_tools.update_start_time(datetime.strftime(start_time, '%Y-%m-%d_%H:%M:%S'))
-            session_tools.update_end_time('1900-01-01_00:00:00')
-            status.text('timer started at ' + datetime.strftime(start_time, '%Y-%m-%d_%H:%M:%S'))
-        if end_button:
-            if session_tools.get_start_time_string() != '1900-01-01_00:00:00':
-                end_time = datetime.now()
-                session_tools.update_end_time(datetime.strftime(end_time, '%Y-%m-%d_%H:%M:%S'))
-
-        # TIMER
-        # if start_button:
-        #     with st.empty():
-        #         done = False
-        #         while not done:
-        #             current_time = datetime.now()
-        #             elapsed_time = (current_time - start_time).total_seconds()
-        #             hours, remainder = divmod(int(elapsed_time), 3600)
-        #             minutes, seconds = divmod(remainder, 60)
-
-        #             st.write('# {:02}:{:02}:{:02}'.format(hours, minutes, seconds))
-
-        #             if end_button:
-        #                 done == True
-
-        #             time.sleep(1)
+    # CONTROL BUTTONS
+    start_button    = st.button('start')
+    end_button      = st.button('end')
+    if start_button:
+        start_time = datetime.now()
+        session_tools.update_start_time(datetime.strftime(start_time, '%Y-%m-%d_%H:%M:%S'))
+        session_tools.update_end_time('1900-01-01_00:00:00')
+        status.text('timer started at ' + datetime.strftime(start_time, '%Y-%m-%d_%H:%M:%S'))
+    if end_button:
+        if session_tools.get_start_time_string() != '1900-01-01_00:00:00':
+            end_time = datetime.now()
+            session_tools.update_end_time(datetime.strftime(end_time, '%Y-%m-%d_%H:%M:%S'))
 
 
     # RESULT
